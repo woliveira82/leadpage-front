8d53ef1e1d6a0d526c7ce50420435fc738702edf
woliveira82@gmail.com <woliveira82@gmail.com>
2021-05-07 23:45:01 -0300

    feat: login basic logic

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 import { Wrapper, LoginBox, P, A } from './styles';
 import { TopBar } from '../../components/TopBar';
-import { Input } from '../../components/Input';
-import { Button } from '../../components/Button';
+import { api } from '../../utils';
+import '../../styles/form.css';
 
-export const Login:React.FC = () => (
-    <Wrapper>
-        <TopBar title="Login" noMenu={ true } />
-        <LoginBox>
-            <h1>Login</h1>
-            <Input placeholder="e-mail"/>
-            <Input type="password" placeholder="password"/>
-            <Button text="LOGIN" />
-            <P>Not registered? <A href="/campaign-list">Create an account</A></P>
-        </LoginBox>
-    </Wrapper>
-);
+export const Login:React.FC = () => {
+    
+    const history = useHistory();
+
+    let [email, setEmail] = useState<string>('');
+    let [password, setPassword] = useState<string>('');
+
+    
+    const login = () => {
+        api.post('/login/', {
+            email: email,
+            password: password
+        })
+        .then((response) => {
+            const token = response.data.data.access_token;
+            localStorage.setItem('keyItemName', token);
+            history.push('/campaign-list')
+        }).catch((error) => {
+            /* console.error(error.data); */
+        });
+    }
+
+    return (
+        <Wrapper>
+            <TopBar title='Login' noMenu={ true } />
+            <LoginBox>
+                <h1>Login</h1>
+                <input placeholder='e-mail'
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input type='password' placeholder='password' 
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button onClick={login}>LOGIN</button>
+                <P>Not registered? <A href='/campaign-list'>Create an account</A></P>
+            </LoginBox>
+        </Wrapper>
+    );
+}
